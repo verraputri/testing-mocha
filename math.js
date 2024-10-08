2d67b6e87ee564f9e5a368ac7ad9a23f51eff64e
verraputri <139258842+verraputri@users.noreply.github.com>
2024-10-08 09:31:15 +0700

    Update math.js

--- math.js
+++ math.js
@@ -1,5 +1,5 @@
 export function tambah(a, b) {
-    if (typeof a === 'string' || typeof b === 'string') throw new Error('Tidak bisa di kali menggunakan string')
+    if (typeof a === 'string' || typeof b === 'string') throw new Error('Tidak bisa di tambah menggunakan string')
     return a + b;
 }
 
@@ -9,12 +9,12 @@ export function kali(a, b) {
 }
 
 export function kurang(a, b) {
-    if (typeof a === 'string' || typeof b === 'string') throw new Error('Tidak bisa di kali menggunakan string')
+    if (typeof a === 'string' || typeof b === 'string') throw new Error('Tidak bisa di kurang menggunakan string')
     return a - b;
 }
 
 export function bagi(a, b) {
-    if (typeof a === 'string' || typeof b === 'string') throw new Error('Tidak bisa di kali menggunakan string')
+    if (typeof a === 'string' || typeof b === 'string') throw new Error('Tidak bisa di bagi menggunakan string')
     if (b === 0) {
         throw new Error('Tidak bisa membagi dengan nol');
     }
